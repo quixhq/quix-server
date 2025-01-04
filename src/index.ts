88644a367d75eedfd2448d672edca5dd50667bf0
yashjawale <yash25.j@gmail.com>
2025-01-04 22:42:31 +0530

    Install Socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('world')
+import { Server as SocketServer } from "socket.io";
+
+const io = new SocketServer(3000, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://slidesyncmobile.web.app"
+    ],
+  },
+})
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
