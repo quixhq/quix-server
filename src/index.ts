13b7c2618b5f26d88df54d2d4d11363641f67a9c
yashjawale <yash25.j@gmail.com>
2025-01-14 12:23:24 +0530

    Fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { Server as SocketServer } from "socket.io";
+import { Server as SocketServer } from 'socket.io'
 
 const io = new SocketServer(3000, {
   cors: {
     origin: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "https://slidesyncmobile.web.app"
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://slidesyncmobile.web.app',
     ],
   },
 })
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("a user disconnected");
-  });
-});
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('a user disconnected')
+  })
+})
